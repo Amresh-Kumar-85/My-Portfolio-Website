2cd2a56965b1952655d487b7d3cfcce9fd327ef0
Amresh-Kumar-85 <amresh444666@gmail.com>
2025-08-01 21:26:26 +0500

    Updated portfolio code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       {" "}
       <p className="text-sm text-muted-foreground">
         {" "}
-        &copy; {new Date().getFullYear()} Amresh Kumar!! All rights reserved.
+        &copy; {new Date().getFullYear()} Amresh Kumar🌷Dashie !! All rights reserved.
       </p>
       <a
         href="#hero"
